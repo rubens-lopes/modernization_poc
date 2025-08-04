d28d3802742a3c858e301c1612bb00fd0558ea7c
Rubens Lopes <github@rubenslop.es>
2025-08-04 16:05:15 -0400

    wip

--- Legacy/Global.asax.cs
+++ Legacy/Global.asax.cs
@@ -1,7 +1,10 @@
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+using ModernizationPoC.Legacy.DAL;
 
 namespace ModernizationPoC.Legacy
 {
@@ -9,6 +12,8 @@ namespace ModernizationPoC.Legacy
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<ApplicationDbContext>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- Legacy/Migrations/Configuration.cs
+++ Legacy/Migrations/Configuration.cs
@@ -1,12 +1,24 @@
 ﻿using System.Data.Entity.Migrations;
 
+using ModernizationPoC.Legacy.DAL;
+using ModernizationPoC.Shared;
+
 namespace ModernizationPoC.Legacy.Migrations
 {
-    internal sealed class Configuration : DbMigrationsConfiguration<DAL.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+        }
+        
+        protected override void Seed(ApplicationDbContext context)
+        {
+            context
+                .Toggles
+                .AddOrUpdate(new ModernizationToggle { AboutPage = true, Id = 1 });
+
+            context.SaveChanges();
         }
     } 
 }
